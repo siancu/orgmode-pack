e45795c618cb952904abb4a0d8f243bc42d295a8
Stelian Iancu <stelian.iancu@gmail.com>
2013-01-14 23:23:54 +0100

    Added the C-cb keybinding to org-mode.

--- config/orgmode-conf.el
+++ config/orgmode-conf.el
@@ -10,3 +10,4 @@
 (add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode))
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
+(define-key global-map "\C-cb" 'org-iswitchb)
